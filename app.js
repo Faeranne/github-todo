0dceceaff7acae44144edf634cd8a0ec8f49fcf1
Blixa Morgan <blixa@projectmakeit.com>
2014-08-29 14:56:12 -0500

    add parseChange

--- app.js
+++ app.js
@@ -6,11 +6,20 @@ app.use(bodyParser())
 
 app.post('/hook', function(req,res){
 	var commits = req.body.commits
+	res.send(200,'{"message":"ok","result":"ok"}');
 	commits.forEach(function(commit, index, commits){
 		console.log(commit.id)
 		console.log(commit.added);
+		commit.added.forEach(function(file, index, added){
+			parseTODOS(req.body.repository.contents_url,file)
+		})
 	});
-	res.send(200,'{"message":"ok","result":"ok"}');
+	
 });
+
+var parseTODOS = function(contentsUrl,file){
+	console.log(contentsUrl);
+	console.log(file);
+}
 
 app.listen(process.env.PORT);
